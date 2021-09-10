0b824410edd60fdd57086c055fba8a0f80f3612c
DaveGomesFortes <DaveGomesFortes@gmail.com>
2021-09-10 15:25:31 +0200

    a

--- Gallery/index.php
+++ Gallery/index.php
@@ -1,6 +1,20 @@
 <?php
     function gallery(){
+        $result = '';
         $directory = 'img';
+        $array = glob($directory."/*.jpg", GLOB_BRACE);
 
+        foreach($array as $key => $imagepath){
+         $result = '<img class="img" src="'.$imagepath.'">';
+        }
+        return $result;
     }
     ?>
+<html lang="en">
+    <head>
+        <title>Gallery</title>
+    </head>
+    <body>
+        <?php gallery() ?>
+    </body>
+</html>
