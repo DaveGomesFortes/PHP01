f072dbb7f953fb12c016cecddd4ecb065aac1199
DaveGomesFortes <DaveGomesFortes@gmail.com>
2021-09-10 12:10:15 +0200

    a

---
+++ Gallery/index.php
@@ -0,0 +1,6 @@
+<?php
+    function gallery(){
+        $directory = 'img';
+
+    }
+    ?>
---
+++ RekenFunctie/index.php
@@ -0,0 +1,9 @@
+<?php
+    function berekening($i){
+        return $i + 1;
+    } ?>
+<html>
+    <body>
+        <?php echo berekening(4) ?>
+    </body>
+</html>
